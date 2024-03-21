aed0ebd111badfb69c73c582a5097020710ecada
Felipe-Takayuki <felipe.takahashi@etec.sp.gov.br>
2024-03-21 18:28:17 -0300

    Event Entities

---
+++ adamas-api/internal/entity/event_entity.go
@@ -0,0 +1,25 @@
+package entity
+
+type Event struct {
+	ID string 
+	Name string
+	OwnerID string
+	OwnerName string
+	Repositories []*RepositoryInEvent
+
+}
+
+type RepositoryInEvent struct {
+	ID string
+	Repository *Repository
+	Locale string
+
+}
+
+type Comment struct {
+	
+	UserID string
+	UserName string
+	Comment string
+
+}

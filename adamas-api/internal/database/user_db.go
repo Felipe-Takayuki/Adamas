e38dfc7e39fc8ecb5fb309a4144323c100a2d9cb
Felipe-Takayuki <felipe.takahashi@etec.sp.gov.br>
2024-03-28 09:11:08 -0300

    add: func getRepositories of the User

--- adamas-api/internal/database/user_db.go
+++ adamas-api/internal/database/user_db.go
@@ -1,6 +1,10 @@
 package database
 
-import "database/sql"
+import (
+	"database/sql"
+
+	"github.com/Felipe-Takayuki/Adamas/adamas-api/internal/entity"
+)
 
 type UserDB struct {
 	db *sql.DB
@@ -11,4 +15,20 @@ func NewUserDB (db *sql.DB) *UserDB {
 	}
 }
 
-func GetRepositories() {}
+func (ud * UserDB) GetRepositories( ) ([]*entity.Repository, error) {
+	rows, err := ud.db.Query("SELECT id, title, description FROM repository")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var repositories []*entity.Repository 
+	for rows.Next() {
+		var repository entity.Repository
+		err := rows.Scan(&repository.ID, &repository.Title, &repository.Description) 
+		if err != nil {
+			return nil, err
+		}
+		repositories = append(repositories, &repository)
+	}
+	return repositories, nil
+}

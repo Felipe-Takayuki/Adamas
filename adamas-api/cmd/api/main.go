f285269ecdc952fec0832ef0a7b91b9f6916aff7
Felipe-Takayuki <felipe.takahashi@etec.sp.gov.br>
2024-03-28 09:10:04 -0300

    test database connection

--- adamas-api/cmd/api/main.go
+++ adamas-api/cmd/api/main.go
@@ -1,1 +1,22 @@
-package main 
+package main
+
+import (
+	"database/sql"
+
+	"github.com/Felipe-Takayuki/Adamas/adamas-api/internal/database"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+	db, err := sql.Open("mysql", "root:@tcp(localhost:3306)/adamas_db")
+	if err != nil {
+		print("ERROR")
+		panic(err.Error())
+	}
+	userDB := database.NewUserDB(db)
+	repositories, err := userDB.GetRepositories()
+	if err != nil {
+		panic(err)
+	}
+	print(repositories[0].ID)
+}

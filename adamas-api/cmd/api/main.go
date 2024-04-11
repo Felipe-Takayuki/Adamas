52c13d5dae0ae279893d9a4587a48fe3dde9ec44
Felipe-Takayuki <felipe.takahashi@etec.sp.gov.br>
2024-04-11 09:45:57 -0300

    routes

--- adamas-api/cmd/api/main.go
+++ adamas-api/cmd/api/main.go
@@ -2,9 +2,14 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
+	"net/http"
 
 	"github.com/Felipe-Takayuki/Adamas/adamas-api/internal/database"
 	"github.com/Felipe-Takayuki/Adamas/adamas-api/internal/service"
+	"github.com/Felipe-Takayuki/Adamas/adamas-api/internal/webserver"
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/chi/v5"
 	_ "github.com/go-sql-driver/mysql"
 )
 
@@ -16,9 +21,14 @@ func main() {
 	}
 	userDB := database.NewUserDB(db)
 	userService := service.NewUserService(*userDB)
-	usr, err := userService.CreateUser("Felipe", "felipe@gmail.com", "12345678")
-	if err != nil {
-		panic(err)
-	}
-	print(usr.Name, " ", usr.Email, " ", usr.Password)
+
+	webUserService := webserver.NewWebUserHandler(*userService)
+
+	c := chi.NewRouter()
+	c.Use(middleware.Logger)
+	c.Use(middleware.Recoverer)
+	c.Post("/create", webUserService.CreateUser)
+	c.Post("/login", webUserService.LoginUser)
+	fmt.Println("Server is running on port 8080")
+	http.ListenAndServe(":8080", c)
 }

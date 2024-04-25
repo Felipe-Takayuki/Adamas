077760842bd7ca93d73dd4006c7775c92e7258da
Felipe-Takayuki <felipe.takahashi@etec.sp.gov.br>
2024-04-25 09:41:42 -0300

    fix: type properties

--- adamas-api/internal/entity/repository_entity.go
+++ adamas-api/internal/entity/repository_entity.go
@@ -1,23 +1,33 @@
 package entity
 
+
 type Bloc struct {
 	SubTitle string
-	Content string
+	Content  string
 }
 
 type Category struct {
-	ID string
+	ID   int
 	Name string
 }
 
-
 type Repository struct {
-	ID string
-	Title string
+	ID          int
+	Title       string
 	Description string
-	OwnersID []string
-	OwnersName []string
-	Categories []*Category
-	Blocs []*Bloc
+	OwnersID    []int
+	OwnersName  []string
+	CategoriesID []int 
+	CategoriesName string
+	Blocs       []*Bloc
 }
 
+func NewRepository(title, description string, ownerID int, categoriesID []int) *Repository {
+	var ownersID []int
+	return &Repository{
+		Title:       title,
+		Description: description,
+		OwnersID:    append(ownersID, ownerID),
+		CategoriesID:  categoriesID,
+	}
+}

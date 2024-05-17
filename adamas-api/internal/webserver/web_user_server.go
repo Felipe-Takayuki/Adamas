fe7e3a4c22718ef8691f96f0f986128c71c74487
Felipe-Takayuki <felipe.takahashi@etec.sp.gov.br>
2024-05-17 09:08:27 -0300

    fix: increase time in jwt token

--- adamas-api/internal/webserver/web_user_server.go
+++ adamas-api/internal/webserver/web_user_server.go
@@ -49,7 +49,7 @@ func (wub *WebUserHandler) CreateUser(w http.ResponseWriter, r *http.Request, to
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	} else {
-		claims := map[string]interface{}{"id": result.Id, "name": result.Name, "email": result.Email, "exp" : jwtauth.ExpireIn(time.Minute * 1)}
+		claims := map[string]interface{}{"id": result.Id, "name": result.Name, "email": result.Email, "exp" : jwtauth.ExpireIn(time.Minute * 10)}
 		_, tokenString, _ = tokenAuth.Encode(claims)
 		json.NewEncoder(w).Encode(map[string]interface{}{
 			"token" : tokenString,
@@ -70,7 +70,7 @@ func (wub *WebUserHandler) LoginUser(w http.ResponseWriter, r *http.Request, tok
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	} else {
-		claims := map[string]interface{}{"id": result.Id, "name": result.Name, "email": result.Email, "exp" : jwtauth.ExpireIn(time.Minute * 1)}
+		claims := map[string]interface{}{"id": result.Id, "name": result.Name, "email": result.Email, "exp" : jwtauth.ExpireIn(time.Minute * 10)}
 		_, tokenString, _ = tokenAuth.Encode(claims)
 		json.NewEncoder(w).Encode(map[string]interface{}{
 			"token" : tokenString,

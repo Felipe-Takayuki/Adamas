839c2679962d04d28cc72a6f8d2fbf48d3d28473
Felipe-Takayuki <felipe.takahashi@etec.sp.gov.br>
2024-05-27 11:38:22 -0300

    fix: cnpj type

--- adamas-api/adamas_db.sql
+++ adamas-api/adamas_db.sql
@@ -10,8 +10,9 @@ CREATE TABLE INSTITUTION_USER(
     name varchar(255) NOT NULL,
     email varchar(255) NOT NULL UNIQUE,
     password varchar(64) NOT NULL,
-    cnpj int NOT NULL
+    cnpj bigint NOT NULL
 );
+
 CREATE TABLE COMMON_USER(
     id int auto_increment NOT NULL PRIMARY KEY,
     name varchar(255) NOT NULL,
@@ -32,13 +33,24 @@ CREATE TABLE OWNERS_REPOSITORY(
     owner_id int NOT NULL REFERENCES COMMON_USER(id),
     PRIMARY KEY(repository_id, owner_id)
 );
-CREATE TABLE BLOC_REPOSITORY(
-    id int NOT NULL auto_increment PRIMARY KEY,
-    repository_id varchar(36) NOT NULL,
-    subtitle varchar(255) NOT NULL,
-    content varchar(255) NOT NULL
+-- CREATE TABLE BLOC_REPOSITORY(
+--     id int NOT NULL auto_increment PRIMARY KEY,
+--     repository_id varchar(36) NOT NULL,
+--     subtitle varchar(255) NOT NULL,
+--     content varchar(255) NOT NULL
+-- );
+CREATE TABLE EVENT(
+    id int auto_increment NOT NULL PRIMARY KEY,
+    institution_id int NOT NULL,
+    name varchar(100) NOT NULL,
+    address varchar(255) NOT NULL,
+    description varchar(255) NOT NULL,
+    FOREIGN KEY (institution_id) REFERENCES INSTITUTION_USER(id)
 );
 
+CREATE TABLE ROOM_IN_EVENT(
+    id 
+)
 CREATE TABLE CATEGORY_REPO(
     category_id int NOT NULL,
     repository_id int NOT NULL,

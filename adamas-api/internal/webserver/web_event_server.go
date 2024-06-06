e7db0ae797df989472f388d863a65c66f4c3f413
Felipe-Takayuki <felipe.takahashi@etec.sp.gov.br>
2024-06-06 08:48:10 -0300

    fix: add verify type user

--- adamas-api/internal/webserver/web_event_server.go
+++ adamas-api/internal/webserver/web_event_server.go
@@ -22,25 +22,41 @@ func NewWebEventHandler(eventService *service.EventService) *WebEventHandler {
 
 func (weh *WebEventHandler) CreateEvent(w http.ResponseWriter, r *http.Request) {
 	_, claims, _ := jwtauth.FromContext(r.Context())
-	flt64, ok := claims["id"].(float64)
+	w.Header().Set("Content-Type", "application/json")
+	userType, ok := claims["user_type"].(string) 
 	if !ok {
-		http.Error(w, "id is not int!", http.StatusInternalServerError)
+		http.Error(w, "id is not exists!", http.StatusInternalServerError)
 		return
 	}
-	var req *reqs.CreateEventRequest
-	err := json.NewDecoder(r.Body).Decode(&req)
-	if err != nil {
-		error := utils.ErrorMessage{Message: err.Error()}
+	if userType == "institution_user" {
+		flt64, ok := claims["id"].(float64)
+		if !ok {
+			http.Error(w, "id is not int!", http.StatusInternalServerError)
+			return
+		}
+		var req *reqs.CreateEventRequest
+		err := json.NewDecoder(r.Body).Decode(&req)
+		if err != nil {
+			error := utils.ErrorMessage{Message: err.Error()}
+			w.WriteHeader(http.StatusBadRequest)
+			json.NewEncoder(w).Encode(error)
+			return
+		}
+        result, err := weh.eventService.CreateEvent(req.Name, req.Address, req.Date, req.Description, int(flt64))
+		if err != nil {
+			error := utils.ErrorMessage{Message: err.Error()}
+			w.WriteHeader(http.StatusInternalServerError)
+			json.NewEncoder(w).Encode(error)
+			return
+		}
+		json.NewEncoder(w).Encode(result)
+	} else {
+		error := utils.ErrorMessage{Message: "este usuário não possui essa permissão"}
 		w.WriteHeader(http.StatusBadRequest)
 		json.NewEncoder(w).Encode(error)
 		return
 	}
-	result, err := weh.eventService.CreateEvent(req.Name, req.Address, req.Date, req.Description, int(flt64))
-	if err != nil {
-		error := utils.ErrorMessage{Message: err.Error()}
-		w.WriteHeader(http.StatusInternalServerError)
-		json.NewEncoder(w).Encode(error)
-		return
-	}
-	json.NewEncoder(w).Encode(result)
+		
+
+
 }
--- adamas-api/internal/webserver/web_repository_server.go
+++ adamas-api/internal/webserver/web_repository_server.go
@@ -3,6 +3,7 @@ package webserver
 import (
 	"encoding/json"
 	"net/http"
+
 	"github.com/Felipe-Takayuki/Adamas/adamas-api/internal/entity"
 	"github.com/Felipe-Takayuki/Adamas/adamas-api/internal/service"
 	"github.com/Felipe-Takayuki/Adamas/adamas-api/internal/utils"
@@ -56,27 +57,39 @@ func (wph *WebRepoHandler) GetRepositories(w http.ResponseWriter, r *http.Reques
 func (wph *WebRepoHandler) CreateRepo(w http.ResponseWriter, r *http.Request) {
 	_, claims, _ := jwtauth.FromContext(r.Context())
 	w.Header().Set("Content-Type", "application/json")
-	flt64, ok := claims["id"].(float64)
+	userType, ok := claims["user_type"].(string) 
 	if !ok {
-		http.Error(w, "id is not int!", http.StatusInternalServerError)
+		http.Error(w, "id is not exists!", http.StatusInternalServerError)
 		return
 	}
-	userID := flt64
-	var req *entity.RepositoryRequestFirst
-	err := json.NewDecoder(r.Body).Decode(&req)
-	if err != nil {
-		error := utils.ErrorMessage{Message: err.Error()}
+	if userType == "common_user" {
+		flt64, ok := claims["id"].(float64)
+		if !ok {
+			http.Error(w, "id is not int!", http.StatusInternalServerError)
+			return
+		}
+		userID := flt64
+		var req *entity.RepositoryRequestFirst
+		err := json.NewDecoder(r.Body).Decode(&req)
+		if err != nil {
+			error := utils.ErrorMessage{Message: err.Error()}
+			w.WriteHeader(http.StatusBadRequest)
+			json.NewEncoder(w).Encode(error)
+			return
+		}
+		result, err := wph.RepoService.CreateRepo(req.Title, req.Description, req.Content, int(userID))
+		if err != nil {
+			error := utils.ErrorMessage{Message: err.Error()}
+			w.WriteHeader(http.StatusInternalServerError)
+			json.NewEncoder(w).Encode(error)
+			return
+		}
+		json.NewEncoder(w).Encode(result)
+	} else {
+		error := utils.ErrorMessage{Message: "este usuário não possui essa permissão"}
 		w.WriteHeader(http.StatusBadRequest)
 		json.NewEncoder(w).Encode(error)
 		return
 	}
-	result, err := wph.RepoService.CreateRepo(req.Title, req.Description,req.Content, int(userID))
-	if err != nil {
-		error := utils.ErrorMessage{Message: err.Error()}
-		w.WriteHeader(http.StatusInternalServerError)
-		json.NewEncoder(w).Encode(error)
-		return
-	}
-	json.NewEncoder(w).Encode(result)
-
+		
 }

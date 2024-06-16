8fdedef4680e43fc2f639ad26c9cbc354220bc98
Felipe-Takayuki <felipe.takahashi@etec.sp.gov.br>
2024-06-16 12:23:39 -0300

    add trigger on db

--- adamas-api/adamas_db.sql
+++ adamas-api/adamas_db.sql
@@ -65,6 +65,22 @@ CREATE TABLE CATEGORY_REPO(
     PRIMARY KEY(category_id,repository_id)
 );
 
+DELIMITER $$
+CREATE TRIGGER limit_category_count
+BEFORE INSERT ON CATEGORY_REPO
+FOR EACH ROW
+BEGIN
+    DECLARE category_count INT;
+    SELECT COUNT(*) INTO category_count 
+    FROM CATEGORY_REPO 
+    WHERE repository_id = NEW.repository_id;
+    IF category_count >= 3 THEN
+        SIGNAL SQLSTATE '45000' 
+        SET MESSAGE_TEXT = 'Limite de 3 categorias por repositório atingido';
+    END IF;
+END$$
+DELIMITER ;
+
 CREATE TABLE CATEGORY(
     id int auto_increment NOT NULL PRIMARY KEY,
     name varchar(200) NOT NULL

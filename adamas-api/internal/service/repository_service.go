6f0cdc06cb657df2cb1c6cc2aaa8307c04650686
Felipe-Takayuki <felipe.takahashi@etec.sp.gov.br>
2024-06-26 10:06:30 -0300

    feat: added deleted repository endpoint

--- adamas-api/internal/service/repository_service.go
+++ adamas-api/internal/service/repository_service.go
@@ -44,6 +44,15 @@ func (rs *RepositoryService) EditRepo(title, description, content string, reposi
 	return repo, nil
 }
 
+func (rs *RepositoryService) DeleteRepo(email, password string, repositoryID int64) error {
+
+	err := rs.RepositoryDB.DeleteRepo(email, password, repositoryID)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
 func (rs *RepositoryService) SetCategory(categoryName string, repoID int64) (error) {
 	err := rs.RepositoryDB.SetCategory(categoryName, repoID)
 	if err != nil {
@@ -52,18 +61,3 @@ func (rs *RepositoryService) SetCategory(categoryName string, repoID int64) (err
 	return err 
 }
 
-func (rs *RepositoryService) SetComment(ownerID, repositoryID int64, comment string) (error) {
-	err := rs.RepositoryDB.SetComment(repositoryID, ownerID, comment) 
-	if err != nil {
-		return err 
-	}
-	return nil
-}
-
-func (rs *RepositoryService) DeleteComment(comment_id, repository_id int64) (error) {
-	err := rs.RepositoryDB.DeleteComment(repository_id, comment_id)
-	if err != nil {
-		return err 
-	}
-	return nil
-}
--- adamas-api/internal/webserver/web_repository_server.go
+++ adamas-api/internal/webserver/web_repository_server.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"net/http"
 	"strconv"
+
 	"github.com/Felipe-Takayuki/Adamas/adamas-api/internal/entity"
 	"github.com/Felipe-Takayuki/Adamas/adamas-api/internal/entity/reqs"
 	"github.com/Felipe-Takayuki/Adamas/adamas-api/internal/service"
@@ -94,6 +95,39 @@ func (wph *WebRepoHandler) CreateRepo(w http.ResponseWriter, r *http.Request) {
 	}
 
 }
+func (wph *WebRepoHandler) DeleteRepo(w http.ResponseWriter, r *http.Request) {
+	_, claims, _ := jwtauth.FromContext(r.Context())
+	w.Header().Set("Content-Type", "application/json")
+	userType, ok := claims["user_type"].(string)
+	if !ok {
+		http.Error(w, "user_type is not string!", http.StatusInternalServerError)
+		return
+	}
+
+	if userType == "common_user" {
+		repoID, err := strconv.Atoi(chi.URLParam(r, "repository_id"))
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		var reqs *reqs.LoginRequest
+		err = json.NewDecoder(r.Body).Decode(&reqs)
+		if err != nil {
+			error := utils.ErrorMessage{Message: err.Error()}
+			w.WriteHeader(http.StatusBadRequest)
+			json.NewEncoder(w).Encode(error)
+			return
+		}
+		err = wph.RepoService.DeleteRepo(reqs.Email, reqs.Password, int64(repoID))
+		if err != nil {
+			error := utils.ErrorMessage{Message: err.Error()}
+			w.WriteHeader(http.StatusInternalServerError)
+			json.NewEncoder(w).Encode(error)
+			return
+		}
+		json.NewEncoder(w).Encode(map[string]interface{}{"deleted_repository":repoID})
+	}
+}
 
 func (wph *WebRepoHandler) EditRepo(w http.ResponseWriter, r *http.Request) {
 	_, claims, _ := jwtauth.FromContext(r.Context())
@@ -165,87 +199,3 @@ func (wph *WebRepoHandler) SetCategory(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func (wph *WebRepoHandler) SetComment(w http.ResponseWriter, r *http.Request) {
-	_, claims, _ := jwtauth.FromContext(r.Context())
-	w.Header().Set("Content-Type", "application/json")
-	userType, ok := claims["user_type"].(string)
-	if !ok {
-		http.Error(w, "user_type is not string!", http.StatusInternalServerError)
-		return
-	}
-	if userType == "common_user" {
-		flt64, ok := claims["id"].(float64)
-		if !ok {
-			http.Error(w, "id is not int!", http.StatusInternalServerError)
-			return
-		}
-		userID := flt64
-
-		repositoryID := chi.URLParam(r, "repository_id")
-		repoID , err := strconv.Atoi(repositoryID)
-		if err != nil {
-			http.Error(w, "repository_id is not string!", http.StatusInternalServerError)
-			return
-		}
-		var reqs *reqs.SetCommentRequest
-		err = json.NewDecoder(r.Body).Decode(&reqs)
-		if err != nil {
-			error := utils.ErrorMessage{Message: err.Error()}
-			w.WriteHeader(http.StatusBadRequest)
-			json.NewEncoder(w).Encode(error)
-			return
-		}
-		err = wph.RepoService.SetComment(int64(userID), int64(repoID), reqs.Comment)
-		if err != nil {
-			error := utils.ErrorMessage{Message: err.Error()}
-			w.WriteHeader(http.StatusInternalServerError)
-			json.NewEncoder(w).Encode(error)
-			return
-		}
-		json.NewEncoder(w).Encode(map[string]interface{}{"Comment": reqs.Comment})
-	} else {
-		error := utils.ErrorMessage{Message: "este usuário não possui essa permissão!"}
-		w.WriteHeader(http.StatusBadRequest)
-		json.NewEncoder(w).Encode(error)
-		return
-	}
-}
-
-func (wph *WebRepoHandler) DeleteComment(w http.ResponseWriter, r *http.Request) {
-	_, claims, _ := jwtauth.FromContext(r.Context())
-	w.Header().Set("Content-Type", "application/json")
-	userType, ok := claims["user_type"].(string)
-	if !ok {
-		http.Error(w, "user_type is not string!", http.StatusInternalServerError)
-		return
-	}
-	if userType == "common_user" {
-		var commentID *reqs.CommentID
-		repositoryID := chi.URLParam(r, "repository_id")
-		repoID, err := strconv.Atoi(repositoryID)
-		if err != nil {
-			http.Error(w, "repository_id is not int!", http.StatusInternalServerError)
-			return
-		}
-		err = json.NewDecoder(r.Body).Decode(&commentID)
-		if err != nil {
-			error := utils.ErrorMessage{Message: err.Error()}
-			w.WriteHeader(http.StatusBadRequest)
-			json.NewEncoder(w).Encode(error)
-			return
-		}
-		err = wph.RepoService.DeleteComment(int64(commentID.CommentID),int64(repoID) )
-		if err != nil {
-			error := utils.ErrorMessage{Message: err.Error()}
-			w.WriteHeader(http.StatusInternalServerError)
-			json.NewEncoder(w).Encode(error)
-			return
-		}
-		json.NewEncoder(w).Encode(map[string]interface{}{"deleted_comment":repoID})
-	} else {
-		error := utils.ErrorMessage{Message: "este usuário não possui essa permissão!"}
-		w.WriteHeader(http.StatusBadRequest)
-		json.NewEncoder(w).Encode(error)
-		return
-	}
-}

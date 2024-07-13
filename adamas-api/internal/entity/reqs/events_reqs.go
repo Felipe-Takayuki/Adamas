20fc058aaf8641809ffbd9a6b6cf96137b8ec9d6
Felipe-Takayuki <felipe.takahashi@etec.sp.gov.br>
2024-07-13 12:25:35 -0300

    new request type

--- adamas-api/internal/entity/reqs/events_reqs.go
+++ adamas-api/internal/entity/reqs/events_reqs.go
@@ -1,8 +1,13 @@
 package reqs
 
 type CreateEventRequest struct {
-	Name          string	`json:"name"`
-	Address       string	`json:"address"`
-	Date          string	`json:"date"`
-	Description   string	`json:"description"`
+	Name        string `json:"name"`
+	Address     string `json:"address"`
+	Date        string `json:"date"`
+	Description string `json:"description"`
+}
+
+type AddRoomRequest struct {
+	Name             string `json:"name"`
+	QuantityProjects int    `json:"quantity_projects"`
 }

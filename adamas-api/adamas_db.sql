3e998b4c5f981b24a4283237295f35a5d763c417
Felipe-Takayuki <felipe.takahashi@etec.sp.gov.br>
2024-07-18 10:12:19 -0300

    db

--- adamas-api/adamas_db.sql
+++ adamas-api/adamas_db.sql
@@ -56,12 +56,6 @@ CREATE TABLE SUBSCRIBERS_EVENT(
   PRIMARY KEY(event_id, user_id)
 );
 
-CREATE TABLE PARTICIPANTS_EVENT(
-  event_id int NOT NULL REFERENCES EVENT(id),
-  user_id  int NOT NULL REFERENCES COMMON_USER(id),
-  PRIMARY KEY(event_id, user_id)
-);
-
 CREATE TABLE ROOM_IN_EVENT(
   id int auto_increment NOT NULL PRIMARY KEY,
   event_id int NOT NULL,
@@ -70,15 +64,67 @@ CREATE TABLE ROOM_IN_EVENT(
   FOREIGN KEY (event_id) REFERENCES EVENT(id) 
 );
 
-CREATE TABLE PROJECT_IN_ROOM(
-  project_id int NOT NULL,
-  room_id int NOT NULL,
-  PRIMARY KEY(project_id, room_id)
+CREATE TABLE PENDING_PROJECT(
+  event_id int NOT NULL REFERENCES EVENT(id),
+  project_id int NOT NULL REFERENCES PROJECT(id),
+  PRIMARY KEY(event_id, project_id)
 );
 
+CREATE TABLE PROJECT_IN_ROOM(
+  room_id int NOT NULL REFERENCES ROOM_IN_EVENT(id),
+  project_id int NOT NULL REFERENCES PROJECT(id),
+  PRIMARY KEY(room_id, project_id)
+);
+
+
+DELIMITER $$
+
+CREATE TRIGGER before_insert_project_in_room
+BEFORE INSERT ON PROJECT_IN_ROOM
+FOR EACH ROW
+BEGIN
+  DECLARE max_projects INT;
+  DECLARE current_projects INT;
+
+  -- Obter o valor de quantity_projects da tabela ROOM_IN_EVENT
+  SELECT quantity_projects INTO max_projects
+  FROM ROOM_IN_EVENT
+  WHERE id = NEW.room_id;
+
+  -- Contar o número atual de projetos na sala
+  SELECT COUNT(*) INTO current_projects
+  FROM PROJECT_IN_ROOM
+  WHERE room_id = NEW.room_id;
+
+  -- Verificar se o limite foi atingido
+  IF current_projects >= max_projects THEN
+    SIGNAL SQLSTATE '45000'
+    SET MESSAGE_TEXT = 'O número máximo de projetos nesta sala foi atingido';
+  END IF;
+END$$
+
+DELIMITER ;
+
+
+
+
+CREATE TABLE CATEGORY(
+  id int auto_increment NOT NULL PRIMARY KEY,
+  name varchar(200) NOT NULL
+); 
+
+INSERT INTO CATEGORY(name) values 
+("Saúde"),
+("Agricultura"),
+("Ferramenta"),
+("Música"),
+("TI"),
+("Marketing"),
+("Mecânica");
+
 CREATE TABLE CATEGORY_PROJECT(
-  category_id int NOT NULL,
-  project_id int NOT NULL,
+  category_id int NOT NULL REFERENCES CATEGORY(id),
+  project_id int NOT NULL REFERENCES PROJECT(id),
   PRIMARY KEY(category_id,project_id)
 );
 
@@ -98,20 +144,7 @@ BEGIN
 END$$
 DELIMITER ;
 
-CREATE TABLE CATEGORY(
-  id int auto_increment NOT NULL PRIMARY KEY,
-  name varchar(200) NOT NULL
-); 
 
-
-INSERT INTO CATEGORY(name) values 
-("Saúde"),
-("Agricultura"),
-("Ferramenta"),
-("Música"),
-("TI"),
-("Marketing"),
-("Mecânica");
 
 CREATE TABLE COMMENT(
   id int auto_increment NOT NULL PRIMARY KEY,

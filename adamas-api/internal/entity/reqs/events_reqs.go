e9f079e85b0beea0a85beb5819fd73717ee82f3d
Felipe-Takayuki <felipe.takahashi@etec.sp.gov.br>
2024-07-21 13:18:51 -0300

    feat: new request type

--- adamas-api/internal/entity/reqs/events_reqs.go
+++ adamas-api/internal/entity/reqs/events_reqs.go
@@ -13,6 +13,10 @@ type AddRoomRequest struct {
 }
 
 type AddPendingProjectRequest struct {
-	UserID 		int64 `json:"user_id"`
 	ProjectID 	int64 `json:"project_id"`
+}
+
+type ApproveProjectRequest struct {
+	ProjectID 	int64 `json:"project_id"`
+	RoomID 		int64 `json:"room_id"`
 }

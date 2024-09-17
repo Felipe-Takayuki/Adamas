e0a7167396c578095f381693639c872ccd223331
etec@devisate.com.br <etec@devisate.com.br>
2024-09-17 08:18:50 -0300

    fix: encripty key in function new user and institution

--- adamas-api/internal/entity/entity.go
+++ adamas-api/internal/entity/entity.go
@@ -1,5 +1,7 @@
 package entity
 
+import "github.com/Felipe-Takayuki/Adamas/adamas-api/internal/utils"
+
 type User struct {
 	ID          int64  `json:"id"`
 	Name        string `json:"name"`
@@ -10,34 +12,32 @@ type User struct {
 	UserType    string `json:"user_type"`
 }
 type Institution struct {
-	ID          int64  `json:"id"`
-	Name        string `json:"name"`
-	NickName    string `json:"nickname"`
-	Description string `json:"description"`
-	Email       string `json:"email"`
-	Password    string `json:"password"`
-	UserType    string `json:"user_type"`
-	CNPJ   string   `json:"cnpj"`
-	Events []*Event `json:"events"`
+	ID          int64    `json:"id"`
+	Name        string   `json:"name"`
+	NickName    string   `json:"nickname"`
+	Description string   `json:"description"`
+	Email       string   `json:"email"`
+	Password    string   `json:"password"`
+	UserType    string   `json:"user_type"`
+	CNPJ        string   `json:"cnpj"`
+	Events      []*Event `json:"events"`
 }
 
 func NewInstitution(name, email, password string, cnpj string) *Institution {
 	return &Institution{
-		Name: name,
-		Email: email,
-		Password: password,
-		CNPJ: cnpj,
+		Name:     name,
+		Email:    email,
+		Password: utils.EncriptKey(password),
+		CNPJ:     cnpj,
 	}
 }
 
-
-
 func NewUser(name, nickName, description, email, password string) *User {
 	return &User{
-		Name: name,
-		NickName: nickName,
+		Name:        name,
+		NickName:    nickName,
 		Description: description,
-		Email: email,
-		Password: password,
+		Email:       email,
+		Password:    utils.EncriptKey(password),
 	}
 }
